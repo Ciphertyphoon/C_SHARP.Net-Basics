67467d22d0f52c7188b867875dbfab8230522b2e
mahesh.18jonna <mahesh.18jonna@gmail.com>
2018-05-19 21:23:06 -0400

    C#.Net Basics

---
+++ InheritanceRectangleApplication/ExecuteRectangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceRectangleApplication
+{
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Tabletop t = new Tabletop(4.5, 7.5);
+            t.Display();
+            Console.ReadLine();
+        }
+    }
+}
