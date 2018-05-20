4c72d2d17386c90d5c3f3580b4ff374262ba45db
mahesh.18jonna <mahesh.18jonna@gmail.com>
2018-05-19 21:12:40 -0400

    C#.NET

---
+++ InheritanceApplication/Rectangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceApplication
+{
+    // Derived class
+    class Rectangle : Shape
+    {
+        public int getArea()
+        {
+            return (width * height);
+        }
+    }
+}
---
+++ InheritanceApplication/RectangleTester.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceApplication
+{
+    class RectangleTester
+    {
+        static void Main(string[] args)
+        {
+            Rectangle Rect = new Rectangle();
+
+            Rect.setWidth(5);
+            Rect.setHeight(7);
+
+            // Print the area of the object.
+            Console.WriteLine("Total area: {0}", Rect.getArea());
+            Console.ReadKey();
+        }
+    }
+}
