7aa8c98d9c123e83b7dcf9c98ae7b9328e6f9d56
mahesh.18jonna <mahesh.18jonna@gmail.com>
2018-05-20 10:43:32 -0400

    Multiple Inheritance won't support

---
+++ MultipleInheritance_InheritanceApplication/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleInheritance_InheritanceApplication
+{
+    // Derived class
+    class Rectangle : Shape, PaintCost
+    {
+        public int getArea()
+        {
+            return (width * height);
+        }
+        public int getCost(int area)
+        {
+            return area * 70;
+        }
+    }
+}
---
+++ MultipleInheritance_InheritanceApplication/RectangleTester.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleInheritance_InheritanceApplication
+{
+    class RectangleTester
+    {
+        static void Main(string[] args)
+        {
+            Rectangle Rect = new Rectangle();
+            int area;
+
+            Rect.setWidth(5);
+            Rect.setHeight(7);
+            area = Rect.getArea();
+
+            // Print the area of the object.
+            Console.WriteLine("Total area: {0}", Rect.getArea());
+            Console.WriteLine("Total paint cost: ${0}", Rect.getCost(area));
+            Console.ReadKey();
+        }
+    }
+}
